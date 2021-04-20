fad9e6bc558068e72c5d10385815a04ed7eb047e
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-04-20 10:19:54 -0500

    Created a Stopwatch class and gave it a Start and a Stop Method along with a private DateTime field _start

---
+++ CSharpIntermediate/Stopwatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIntermediate
+{
+    class Stopwatch
+    {
+        private DateTime _start;
+
+
+        public void Start()
+        {
+            _start = DateTime.Now;
+        }
+
+        public void Stop()
+        {
+            TimeSpan duration = DateTime.Now - _start;
+            Console.WriteLine(duration.TotalSeconds.ToString());
+        }
+    }
+}
