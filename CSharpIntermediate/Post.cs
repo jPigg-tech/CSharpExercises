cbdbdc848593e498685f25c7f7dbc7c8cd400a33
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-04-20 20:17:38 -0500

    Made the Votes list readonly and added a property for VoteCoun. Also created a method to get a posts vote count.

--- CSharpIntermediate/Post.cs
+++ CSharpIntermediate/Post.cs
@@ -11,7 +11,8 @@ namespace CSharpIntermediate
         public string Title { get; set; }
         public string  Description { get; set; }
         public DateTime DateCreated { get; set; }
-        private List<int> Votes = new List<int>();
+        public int VoteCount { get; set; }
+        private readonly List<int> Votes = new List<int>();
 
         public void VoteUp()
         {
@@ -23,6 +24,11 @@ namespace CSharpIntermediate
             Votes.Remove(1);
         }
 
+        public int GetVoteCount()
+        {
+            VoteCount = Votes.Count();
+            return VoteCount;
+        }
 
     }
 }
