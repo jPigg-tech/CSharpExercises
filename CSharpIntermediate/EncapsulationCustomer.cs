41e340b2c265a116573fa041b26c941c481ace45
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-04-26 14:34:31 -0500

    Created a EncapsulatedCustomers Class and gave it some feilds and methods to show Encapsulation

---
+++ CSharpIntermediate/EncapsulationCustomer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIntermediate
+{
+    class EncapsulationCustomer
+    {
+        public int Id { get; set; }
+        public string  Name { get; set; }
+
+        public void Promote()
+        {
+            var rating = CalculateRating(excludeOrders: true);
+            if (rating == 0)
+            {
+                Console.WriteLine("Promoted to level 1");
+            }
+            Console.WriteLine("Promoted to level 2");
+        }
+
+        private int CalculateRating(bool excludeOrders)
+        {
+            return 0;
+        }
+    }
+}
