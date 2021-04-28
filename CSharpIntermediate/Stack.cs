1e74a842e133c529107052889ff02bfee71b0365
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-04-28 14:52:21 -0500

    Created a Push and a Pop method for in Stack.cs

--- CSharpIntermediate/Stack.cs
+++ CSharpIntermediate/Stack.cs
@@ -8,5 +8,40 @@ namespace CSharpIntermediate
 {
     class Stack
     {
+        private readonly List<object> objs = new List<object>();
+
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+            {
+                throw new InvalidOperationException();
+            }
+            objs.Add(obj);
+
+        }
+
+        public object Pop()
+        {
+            var popObj = new object();
+            if (objs.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            for (int i = objs.Count - 1; i < 0; i--)
+            {
+                popObj = objs[i];
+                objs.Remove(objs[i]);
+                return popObj;
+            }
+
+            return popObj;
+        }
+
+        public void Clear()
+        {
+
+        }
     }
 }
