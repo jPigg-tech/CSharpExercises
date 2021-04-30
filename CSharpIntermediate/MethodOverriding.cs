1dd4a6c8e88b73baf2d78923455332697e77ff18
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-04-30 11:11:34 -0500

    Created a Canvas class that have a method that takes in a list of shapes and iterates over the list. It doesn't what shape as long it is a shape or a child of the shape

--- CSharpIntermediate/MethodOverriding.cs
+++ CSharpIntermediate/MethodOverriding.cs
@@ -26,5 +26,18 @@ namespace CSharpIntermediate
                 Console.WriteLine("Draw a circle");
             }
         }
+        public class Rectangle : Shape
+        {
+            public override void Draw()
+            {
+                Console.WriteLine("Draw a rectangle");
+            }
+        }
+
+        // We can create a triangle class and it would have not effect
+        // on the shape class circle or any other class 
+        // with polymorphism we can simply create new shapes that have no 
+        // impact on our exsisting classes, this is also a sign of 
+        // loosely coupled classes 
     }
 }
