49ce8ba5b814e1ea2116c29bdfafcc3162afd57c
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-04-30 21:35:33 -0500

    Implemented some flow control, controlling if the instruction is to go to the SQL server or Oracle Server

--- CSharpIntermediate/DbCommand.cs
+++ CSharpIntermediate/DbCommand.cs
@@ -8,13 +8,27 @@ namespace CSharpIntermediate
 {
     class DbCommand
     {
-
+        
         public DbCommand(DbConnection dbConnection)
         {
             if (dbConnection == null)
             {
                 throw new ArgumentNullException();
-            }            
+            }
+            else if (dbConnection.ConnectionString == "SQL")
+            {
+                Console.WriteLine("Connecting you to the SQL database.");
+            }
+            else if (dbConnection.ConnectionString == "Oracle")
+            {
+                Console.WriteLine("Connecting you to the Oracle database.");
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
         }
+
+
     }
 }
