da9e2496e8f61d7de9d7b3c5c54d731253ad4361
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-05-03 17:46:12 -0500

    Implemented a OrderProcessor test in the test class to test if an item is already shipped the method will return an IvalidOperationException

--- CSharpIntermediate.UnitTests/UnitTest1.cs
+++ CSharpIntermediate.UnitTests/UnitTest1.cs
@@ -4,11 +4,28 @@ using System;
 namespace CSharpIntermediate.UnitTests
 {
     [TestClass]
-    public class UnitTest1
-    {
+    public class OrderProcessorTests
+    { 
+        // MethodName_Condition_Expectation
+
         [TestMethod]
-        public void TestMethod1()
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Process_OrderIsAlreadyShipped_ThrowsAnException()
         {
+            var orderProcessor = new OrderProcessor(new FakeShippingCalculator());
+            var order = new Order()
+            {
+                Shipment = new Shipment()
+            };
+            orderProcessor.Process(order);
+        }
+
+        public class FakeShippingCalculator : IShippingCalculator
+        {
+            public float CalculateShipping(Order order)
+            {
+                return 1;
+            }
         }
     }
 }
--- CSharpIntermediate/OrderProcessor.cs
+++ CSharpIntermediate/OrderProcessor.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CSharpIntermediate
 {
-    class OrderProcessor
+    public class OrderProcessor
     {
         private readonly IShippingCalculator _shippingCalculator;
 
