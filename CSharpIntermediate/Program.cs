264cd44cd38013ad0f8c691dab3353fa17120f50
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-05-03 17:29:12 -0500

    Tested the build of the project in the program class

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -8,7 +8,9 @@ namespace CSharpIntermediate
     {
         static void Main(string[] args)
         {
-            
+            var orderProcessor = new OrderProcessor(new ShippingCalculator());
+            var order = new Order { DatePlaced = DateTime.Now, TotalPrice = 100f };
+            orderProcessor.Process(order);
         }
         static void TestingDbCommandClassShowingPolymorphism()
         {
